d18f4ce0da63fc697ebf8165cc0a922c7b357568
RisingGeek <bhavaypuri15@gmail.com>
2020-11-15 11:55:06 +0530

    Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('yo! this is me')
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to typescript boilerplate');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
